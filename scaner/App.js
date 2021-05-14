21b62709741739e388487098c1f0b99578562c0e
BLOODYmary111 <72738648+BLOODYmary111@users.noreply.github.com>
2021-05-14 11:43:32 +0530

    Add files via upload

--- scaner/App.js
+++ scaner/App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text,View,TouchableOpacity,StyleSheet,Image} from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import sc from './assets/sc.png'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class TransactionScreen extends React.Component{
+    constructor()
+    {
+        super();
+        this.state={
+            hasCameraPermissions:null,
+            scaned:false,  
+            scannedData:" ",
+            buttonState:'normal'
+        }
+    }
+    getCameraPermissions=async()=>{
+        const {status}=await Permissions.askasync(Permissions.CAMERA);
+        this.setState({
+            hasCameraPermissions:status==='granted',
+            buttonState:'clicked'
+        })
+    }
+    render()
+    {
+        const hasCameraPermissions=this.state.hasCameraPermissions;
+        return(
+            <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+              
+      <Image source={sc} style={{ width: 305, height: 159 }} /> 
+            <Text>scanner</Text>
+
+            <TouchableOpacity
+            onPress={this.getCameraPermissions}
+            style={styles.scanButton}>
+                   <Text>Scan QR Code</Text>
+                </TouchableOpacity>
+            </View>
+                    )
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles=StyleSheeet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    displayText:{
+        fontSize:50,
+        textdecorationline:'underline'
+    },
+    scanButton:{
+        backgroundcolor: "red",
+        margin:10,
+        padding:100
+    }
+})
